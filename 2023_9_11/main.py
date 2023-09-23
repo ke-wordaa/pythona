acec5f65dbc958c59e8f5a70f116dcaa99c7a9f1
kevinjj <98082057+ke-wordaa@users.noreply.github.com>
2023-09-23 11:34:41 +0800

    01

--- 2023_9_11/main.py
+++ 2023_9_11/main.py
@@ -1,18 +1,14 @@
 import time , os
-class mytime:
-    def timew():     
-        i = 0
-        minn =60
-        os.system("cls")
-        os.system("color a")
-        print("計時開始!")
-        while True:
-            i += 1
-            if i % minn == 0:
-                mina = int(i/ minn)
-                print(mina,"分鐘")
-                return(mina,"分鐘")
-            else:
-                print(i)
-                return(i)
-            time.sleep(1)
+i = 0
+minn =60
+os.system("cls")
+os.system("color a")
+print("計時開始!")
+while True:
+    i += 1
+    if i % minn == 0:
+        mina = int(i/ minn)
+        print(mina,"分鐘")
+    else:
+        print(i)
+    time.sleep(1)
--- 2023_9_11/time_tk.py
+++ 2023_9_11/time_tk.py
@@ -1,17 +1,27 @@
 from tkinter import *
-from main import mytime
-import time 
-def gettime():
-    get = mytime.timew()
-    return(get)
-print(gettime())
-time.sleep(1)
-print(gettime())
-# root = Tk()
-# root.title("main")
-# root.geometry("200x200")
-# time_le= Label(root,text="時間計時",font=((""),(30)))
-# time_le.pack()
-# time_le_text= Label(root,text="",font=((""),(30)))
-# time_le_text.pack()
-# root.mainloop()
+import time,os
+root = Tk()
+root.title("main")
+root.geometry("200x200")
+time_le= Label(root,text="時間計時",font=((""),(30)))
+time_le.pack()
+def mytime():
+    i = 0
+    minn =60
+    os.system("cls")
+    os.system("color a")
+    print("計時開始!")
+    while True:
+        i += 1
+        if i % minn == 0:
+            mina = int(i/ minn)
+            print(mina,"分鐘")
+            time_le_text['text']=mina,"分鐘"
+        else:
+            time_le_text['text']=i
+            print(i)
+        time.sleep(1)
+time_le_text= Label(root,text="",font=((""),(30)))
+time_le_text.pack()
+root.mainloop()
+mytime()
