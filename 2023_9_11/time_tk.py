4035947aface252135d3c722f3ca3b476179952d
kevinjj <98082057+ke-wordaa@users.noreply.github.com>
2023-09-25 11:54:40 +0800

    112_9_25

--- 2023_9_11/time_tk.py
+++ 2023_9_11/time_tk.py
@@ -1,27 +1,38 @@
 from tkinter import *
-import time,os
+import time,os,math
 root = Tk()
 root.title("main")
 root.geometry("200x200")
 time_le= Label(root,text="時間計時",font=((""),(30)))
 time_le.pack()
+i = 0
+s=False
 def mytime():
-    i = 0
+    global i,s
     minn =60
-    os.system("cls")
-    os.system("color a")
-    print("計時開始!")
-    while True:
+    if s== True:
         i += 1
-        if i % minn == 0:
-            mina = int(i/ minn)
-            print(mina,"分鐘")
-            time_le_text['text']=mina,"分鐘"
-        else:
-            time_le_text['text']=i
-            print(i)
-        time.sleep(1)
+        a=math.trunc(i/minn)
+        b= i%minn
+        print(a,b)
+        time_le_text['text']="%s分%s秒" %(a,b)
+        root.after(1000,mytime)
+    else:
+        time_le_text['text']="%s分%s秒" %(0,0)
+def bt_1_f():
+    global s
+    if s==True:
+        bt_1['text']="開始計時"
+        s=False
+        mytime()
+    else:
+        bt_1['text']="停止計時"
+        s=True
+        mytime()
+
 time_le_text= Label(root,text="",font=((""),(30)))
+
 time_le_text.pack()
+bt_1= Button(root,text="開始計時",font=((""),(30)),command=bt_1_f)
+bt_1.pack()
 root.mainloop()
-mytime()
